d5f43ee3a3c396c43d9620952ca99631c442fb23
Thiago130589 <thiagoferreira2flores@gmail.com>
2025-10-04 01:04:52 -0300

    atualização do gerenciar tarefas

--- firebase-init.js
+++ firebase-init.js
@@ -8,14 +8,15 @@
 
 // 1. Configurações
 const firebaseConfig = {
-    apiKey: "AIzaSyDsYy-C_wQXdLOe08mOTczq63Q_DXky2BM",
-    authDomain: "familia-flores-2ed6a.firebaseapp.com",
-    projectId: "familia-flores-2ed6a",
-    storageBucket: "familia-flores-2ed6a.firebasestorage.app",
-    messagingSenderId: "102151517349",
-    appId: "1:102151517349:web:0fa6cfa865f9da338f494c",
-    measurementId: "G-8E95B0VFNR"
+  apiKey: "AIzaSyDsYy-C_wQXdLOe08mOTczq63Q_DXky2BM",
+  authDomain: "familia-flores-2ed6a.firebaseapp.com",
+  projectId: "familia-flores-2ed6a",
+  storageBucket: "familia-flores-2ed6a.firebasestorage.app",
+  messagingSenderId: "102151517349",
+  appId: "1:102151517349:web:0fa6cfa865f9da338f494c",
+  measurementId: "G-8E95B0VFNR"
 };
+
 
 // Variáveis globais (Usamos 'let' sem escopo local para que outros scripts possam acessá-las)
 let app;
